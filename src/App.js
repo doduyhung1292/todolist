f7704e86a6146822a9744bd4380c698b161136c6
doduyhung1292 <doduyhung1292@gmail.com>
2019-09-07 18:29:28 +0700

    done

--- src/App.js
+++ src/App.js
@@ -6,20 +6,47 @@ import TodoItems from './Components/TodoItems';
 class App extends Component {
   constructor () {
     super();
-    this.todoItems = [
-    'Đổ xăng',
-    'Nấu cơm',
-    'Thịt gà'
-   ];
-
+    this.state = {
+      todoItems: [
+    {title: 'Đổ xăng', isComplete: true},
+    {title: 'Nấu cơm', isComplete: true},
+    {title: 'Rửa bát', isComplete: false}
+     ]
+    }
   }
-   
+  onItemClicked(item) {   
+    return (event) => {
+      const isComplete = item.isComplete;
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+         ...todoItems.slice(0, index),
+         {
+          ...item,
+          isComplete: !isComplete
+         },
+         ...todoItems.slice(index+1)
+        ]
+      })
+
+    }
+  }
 
  render (){
+  const {todoItems} = this.state;
    return (
      <div className="App">
      {
-      this.todoItems.map((item, index) => <TodoItems key={index} title={item} />)
+      todoItems.length > 0 && todoItems.map((item, index) => 
+        <TodoItems 
+        key={index}
+        item={item}
+        onClick = {this.onItemClicked(item)}
+        />)
+     }
+     {
+      todoItems.length === 0 && 'Nothing.'
      }
      </div>
     )
--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import './TodoItems.css';
+
+
 
 class TodoItems extends Component {
   render() {
+  	const {item} = this.props;
+  	let className = 'TodoItems';
+  	if (item.isComplete) {
+       className += ' TodoItems-complete';
+  	}
   	return (
-      <div className="TodoItems">
-        <p>{this.props.title}</p>
+      <div className={className}>
+        <p>{this.props.item.title}</p>
       </div>  
   	)
   }
